9dd537e77492a287e9a9b15e68a7148a709665b3
ziya <ziya@ut.ee>
2022-02-15 17:38:25 +0200

    some changes

--- src/myapp.py
+++ src/myapp.py
@@ -1,14 +1,12 @@
 from flask import Flask
 
-app=Flask("__name__")
+app = Flask("__name__")
+
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
-# @app.route("/ping")
-# def pong():
-#     return "pong"
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,5 @@
 from myapp import index
-from myapp import pong
+
 
 def test_index():
     assert index() == "Hello, World!"
-
-# def test_pong():
-#     assert pong() == "pong"
