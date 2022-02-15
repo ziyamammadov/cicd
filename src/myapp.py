add67d430cc95fe96cf6f868d70319362566089b
ziya <ziya@ut.ee>
2022-02-15 16:50:20 +0200

    some changes

--- src/myapp.py
+++ src/myapp.py
@@ -6,5 +6,9 @@ app=Flask("__name__")
 def index():
     return "Hello, World!"
 
+@app.route("/ping")
+def pong():
+    return "pong"
+
 if __name__=="__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
 from myapp import index
+from myapp import pong
 
 def test_index():
     assert index() == "Hello, World!"
+
+def test_pong():
+    assert pong() == "pong"
